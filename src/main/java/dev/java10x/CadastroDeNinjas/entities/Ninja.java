a0388239015d530603ec6a4a6328697a60d06c39
Anderson Alves <andersonmsalves@gmail.com>
2025-06-13 19:51:19 -0300

    fix: Deixado unique o email do Ninja

--- src/main/java/dev/java10x/CadastroDeNinjas/entities/Ninja.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/entities/Ninja.java
@@ -2,12 +2,18 @@ package dev.java10x.CadastroDeNinjas.entities;
 
 // Jpa = Java Persistence API
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "tb_ninjas")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data // Criar os Getters and Setters
 public class Ninja {
 
     @Id
@@ -16,6 +22,7 @@ public class Ninja {
 
     private String nome;
 
+    @Column(unique = true)
     private String email;
 
     private int idade;
@@ -24,61 +31,4 @@ public class Ninja {
     @ManyToOne
     @JoinColumn(name = "missao_id") // Foreing Key ou chave estrangeira
     private Missao missao;
-
-    public Ninja(){
-
-    }
-
-    public Ninja(String nome, String email, int idade){
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public Ninja(String nome, String email, int idade, Missao missao){
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-        this.missao = missao;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Missao getMissao() {
-        return missao;
-    }
-
-    public void setMissao(Missao missao) {
-        this.missao = missao;
-    }
 }
