88f85ef2b37774b2bb86c4bf2bfa34c136fa81c7
Anderson Alves <andersonmsalves@gmail.com>
2025-06-14 11:37:13 -0300

    fix: adiciona o @Column aos atributos da classe Ninja

--- src/main/java/dev/java10x/CadastroDeNinjas/entities/Ninja.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/entities/Ninja.java
@@ -20,11 +20,16 @@ public class Ninja {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name= "nome")
     private String nome;
 
     @Column(unique = true)
     private String email;
 
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @Column(name = "idade")
     private int idade;
 
     // Um ninja tem uma unica missao
